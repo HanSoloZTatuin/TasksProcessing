108ffd3c32dd58764cea6ad7f9bf63171863179a
greg.greg <terminacja.zadan@op.pl>
2024-01-16 23:27:18 +0100

    update Task, Controller and Service

--- tasks-processing/src/main/java/com/example/tasks/processing/service/TaskService.java
+++ tasks-processing/src/main/java/com/example/tasks/processing/service/TaskService.java
@@ -1,7 +1,49 @@
 package com.example.tasks.processing.service;
 
 
+import com.example.tasks.processing.dto.Task;
+import com.example.tasks.processing.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TaskService  {
+
+    private TaskRepository taskRepository;
+
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long taskId) {
+        return taskRepository.findById(taskId);
+    }
+
+    public Optional<Task> updateTask(Long taskId, Task updatedTask) {
+        Optional<Task> existingTask = taskRepository.findById(taskId);
+        if (existingTask.isPresent()) {
+            Task task = existingTask.get();
+            task.setTitle(updatedTask.getTitle());
+            task.setDescription(updatedTask.getDescription());
+            task.setStatus(updatedTask.getStatus());
+            return Optional.of(taskRepository.save(task));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public boolean deleteTask(Long taskId) {
+        if (taskRepository.existsById(taskId)) {
+            taskRepository.deleteById(taskId);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
